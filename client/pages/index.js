ff2efc2e464827ed043d87e2ad1a48a424fdd3d6
Ethan <ebonsignori@gmail.com>
2019-07-23 14:46:04 -0400

    remove ContactQuery

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,9 @@
 import Layout from '../components/Layout'
-import ContactQuery from '../components/ContactQuery'
 
 const Index = () => (
   <Layout title='Newtnotes | Home'>
     <div>
       <h1>Welcome to Newtnotes</h1>
-      <ContactQuery />
     </div>
   </Layout>
 )
