049219c868f944abc21a63796547500eeb1a1282
EthanB <ebonsignori@gmail.com>
2019-07-27 14:08:50 -0400

    style contacts

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -3,10 +3,51 @@ import React, { Component } from 'react'
 class Contact extends Component {
   render () {
     return (
-      <div style={{ border: '1px solid #777', marginTop: '-1px' }}>
-        <p>{this.props.contact.name}</p>
-        <p>{this.props.contact.email}</p>
-        <p>{this.props.contact.phone}</p>
+      <div className='contact' key={this.props.contact._id}>
+        <div className='profile-pic'>
+          <img src={this.props.contact.profilePicture} alt='profile-pic' />}
+        </div>
+        <div className='text-center'>
+          <p className='lead'>{this.props.contact.name}</p>
+        </div>
+
+        <style jsx>{`
+          .contact {
+            margin: 0.5rem;
+            background-color: #444;
+            border-radius: 4px;
+            padding: 0.75rem;
+          }
+          
+          .contact:hover {
+            background-color: #333;
+          }
+
+          .contact:active {
+            background-color: #222;
+          }
+
+          .profile-pic {
+            display: inline;
+          }
+          
+          div .profile-pic > img {
+            width: 60px;
+            height: 60px;
+            border-radius: 50px;
+            border: 1px solid #CFD0D2;
+          }
+
+          div .text-center {
+            display: inline;
+            color: #fff;
+          }
+
+          div .lead {
+            margin-left: 10px;
+            display: inline;
+          }
+        `}</style>
       </div>
     )
   }
--- client/components/ContactLetterHead.js
+++ client/components/ContactLetterHead.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class ContactLetterHead extends Component {
   render () {
     return (
-      <div>
+      <div key={this.props.letter}>
         <p className='lead'>
           <b>{this.props.letter}</b>
         </p>
