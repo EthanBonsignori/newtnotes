4c144b9266dd34915d5d544346c620cacdc19e89
EthanB <ebonsignori@gmail.com>
2019-07-27 14:09:10 -0400

    add search bar to contacts

--- client/pages/contacts.js
+++ client/pages/contacts.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import Highlighter from 'react-highlight-words'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 import MdPerson from '@material-ui/icons/person'
+import MdSearch from '@material-ui/icons/search'
 import Layout from '../components/Layout'
 import ContactLetterHead from '../components/ContactLetterHead'
 import Contact from '../components/Contact'
@@ -13,33 +17,63 @@ class Contacts extends Component {
 
     this.state = {
       contacts: [],
-      groupedContacts: {}
+      search: ''
     }
+
+    this.handleChangeSearch = this.handleChangeSearch.bind(this)
   }
 
   async componentDidMount () {
     const contacts = await API.getContacts()
     this.setState({ contacts })
-    // const groupedContacts = await groupContacts(this.state.contacts)
-    // this.setState({ groupedContacts })
+  }
+
+  handleChangeSearch (event) {
+    this.setState({ search: event.target.value.toLowerCase() })
   }
 
   render () {
     let currentLetter = ''
+    let contactNames = []
     const contactsView = []
     const contacts = this.state.contacts
-    for (const contact in contacts) {
-      const letter = contacts[contact].name[0].toUpperCase()
-      if (letter !== currentLetter) {
-        currentLetter = letter
-        contactsView.push(<ContactLetterHead key={letter} letter={letter} />)
+    for (const i in contacts) {
+      const contact = contacts[i]
+      contactNames.push(contact.name)
+      if (contact.name.toLowerCase().includes(this.state.search)) {
+        const letter = contact.name[0].toUpperCase()
+        if (letter !== currentLetter) {
+          currentLetter = letter
+          contactsView.push(<ContactLetterHead key={letter} letter={letter} />)
+        }
+        contactsView.push(<Contact key={contact._id} contact={contact} />)
       }
-      contactsView.push(<Contact contact={contacts[contact]} />)
     }
+    contactNames = contactNames.join(' ')
     return (
       <Layout title='Newtnotes | Contacts'>
         {this.state.contacts
-          ? <div>{ contactsView }</div>
+          ? <div>
+            <InputGroup className='mb-3'>
+              <InputGroup.Prepend>
+                <InputGroup.Text id='inputGroup-sizing-default'>
+                  <MdSearch />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                type='text'
+                placeholder='Search Contacts...'
+                value={this.state.MdSearch}
+                onChange={this.handleChangeSearch}
+              />
+            </InputGroup>
+            {/* <Highlighter
+              searchWords={[this.state.search]}
+              textToHighlight={contactNames}
+              autoEscape> */}
+            { contactsView }
+            {/* </Highlighter> */}
+          </div>
           : <div className='text-center mt-5'>
             <p className='display-3'>Uh oh! No contacts found.</p>
             <p className='lead' style={{ fontSize: '3rem' }}>Try creating one below.</p>
