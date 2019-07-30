8e1b4b9ed03973329829eba19576bab3314bd65f
Ethan <ebonsignori@gmail.com>
2019-07-30 01:15:07 -0400

    update contact schema with new fields

--- models/Contact.model.js
+++ models/Contact.model.js
@@ -2,23 +2,28 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const contactSchema = new Schema({
+  imageUrl: String,
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
-  nickname: {
-    type: String
-  },
-  phone: {
-    type: String
-  },
-  email: {
-    type: String
-  },
-  profilePicture: {
-    type: String,
-    default: 'https://i.imgur.com/U2lpZIk.jpg'
-  },
+  prefix: String,
+  suffix: String,
+  firstName: String,
+  lastName: String,
+  nickname: String,
+  email: String,
+  phone: String,
+  facebook: String,
+  twitter: String,
+  instagram: String,
+  linkedin: String,
+  address: String,
+  work: String,
+  relationship: String,
+  birthday: Date,
+  notes: String,
   journalLinks: [{ type: Schema.Types.ObjectId, ref: 'Journal' }]
 })
 
