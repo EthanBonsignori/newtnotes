9199d10e11a04a86c8eb22e6fc090c33b6796af1
Ethan <ebonsignori@gmail.com>
2019-07-30 01:13:23 -0400

    import css, remove global jsx

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import Head from 'next/head'
 import Container from 'react-bootstrap/Container'
 import MyNavbar from './MyNavbar'
-import globalStyles from '../config/globalStyles'
 import PlusButton from '../components/PlusButton'
 import API from '../utils/API'
 import * as actions from '../actions/userActions'
@@ -20,7 +19,7 @@ class Layout extends Component {
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta charSet='utf-8' />
           <title>{this.props.title}</title>
-          {globalStyles()}
+          <link rel='stylesheet' href='/static/styles.css' />
           <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.6/quill.snow.css' />
           <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' />
         </Head>
