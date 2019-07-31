8d51c03eaca49bdbcf618adfb046736d73a65fe4
EthanB <ebonsignori@gmail.com>
2019-07-30 21:11:07 -0400

    add bootstrap navbar

--- client/components/MyNavbar.js
+++ client/components/MyNavbar.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 class MyNavbar extends Component {
   render () {
     return (
-      <nav>
-        <Link href='/'><a>Newtnotes</a></Link>
-        <Link href='/journal'><a>Journal</a></Link>
-        <Link href='/contacts'><a>Contacts</a></Link>
-        <div className='auth-buttons'>
-          <Link href='/auth'><a>Login</a></Link>
-        </div>
-        <style jsx>{`
-          nav {
-            background-color: #222;
-            padding: 1rem;
-          }
-          nav a {
-            margin: 0 1rem;
-            text-decoration: none;
-            color: white
-          }
-
-          .auth-buttons {
-            float: right;
-          }
-        `}</style>
-      </nav>
+      <>
+        <Navbar bg='dark' variant='dark'>
+          <Navbar.Brand href=''>
+            <img
+              alt=''
+              src='/static/favicon.ico'
+              width='30'
+              height='30'
+              className='d-inline-block align-top'
+            />
+            {' Newtnotes'}
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Item>
+              <Link href='/journal' passHref>
+                <Nav.Link>Journal</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href='/contacts' passHref>
+                <Nav.Link>Contacts</Nav.Link>
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+      </>
     )
   }
 }
