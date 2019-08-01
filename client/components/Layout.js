d24a010f49edad62a8653fd5efe2c7b72c7a2cbe
Ethan <ebonsignori@gmail.com>
2019-08-01 04:54:47 -0400

    conditional added to floating-button-menu

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -32,7 +32,7 @@ class Layout extends Component {
             {this.props.children}
           </div>
         </Container>
-        <PlusButton />
+        {this.props.floatingButton ? <PlusButton /> : null}
       </>
     )
   }
