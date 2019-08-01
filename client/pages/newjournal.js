af1749ff234d25727399d9f7c4e87ad7252b3013
Ethan <ebonsignori@gmail.com>
2019-08-01 03:46:52 -0400

    add getInitalProps from URL

--- client/pages/newjournal.js
+++ client/pages/newjournal.js
@@ -1,10 +1,19 @@
 import Layout from '../components/Layout'
 import JournalEditor from '../components/JournalEditor'
+import API from '../utils/API'
 
-const NewJournal = () => (
+const NewJournal = props => (
   <Layout title='Newtnotes | Entry'>
-    <JournalEditor />
+    <JournalEditor journal={props.journal} id={props.id} title={props.title} />
   </Layout>
 )
+
+NewJournal.getInitialProps = async ({ query }) => {
+  if (query && query.id) {
+    const res = await API.getJournal(query.id)
+    return { journal: res.journal, title: res.title, id: res._id }
+  }
+  return { }
+}
 
 export default NewJournal
