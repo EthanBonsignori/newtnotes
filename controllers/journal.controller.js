54c988ec1d2ad367913cf3dd2a69007a4ed38c57
Ethan <ebonsignori@gmail.com>
2019-08-01 03:47:51 -0400

    findOne, update, and sort to find

--- controllers/journal.controller.js
+++ controllers/journal.controller.js
@@ -4,11 +4,27 @@ const findContactLinks = require('../utils/journalParser')
 const journal = {
   findAll: (req, res) => {
     try {
-      Journal.find((err, entries) => {
-        if (!entries) return res.status(404).json({ message: 'No saved entries found.' })
-        if (err) return res.status(400).json({ message: `${err.name} | Error retrieving saved entries` })
-        res.status(200).json(entries)
-      })
+      Journal.find()
+        .sort({ updatedAt: -1 })
+        .exec((err, entries) => {
+          if (!entries) return res.status(404).json({ message: 'No saved entries found.' })
+          if (err) return res.status(400).json({ message: `${err.name} | Error retrieving saved entries` })
+          res.status(200).json(entries)
+        })
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  findOne: (req, res) => {
+    const id = req.params.id
+    try {
+      Journal.findById(id)
+        .populate('contactLinks')
+        .exec((err, journal) => {
+          if (err) return res.status(404)
+          res.status(200).json(journal)
+        })
     } catch (err) {
       console.log(err)
     }
@@ -22,15 +38,51 @@ const journal = {
       contactLinks
     })
     try {
-      newJournal.save(async (err, journal) => {
+      newJournal.save({ new: true }, async (err, journal) => {
         if (err) return console.log(err)
         Contact.updateMany({ _id: { $in: contactLinks } }, { $push: { journalLinks: journal._id } }, { multi: true }, (err, contacts) => {
           if (err) return console.log(err)
+          res.status(201).json({ journal, contacts })
         })
       })
     } catch (err) {
       console.log(err)
       res.status(400).json({ err: err.message })
+    }
+  },
+
+  update: async (req, res) => {
+    const id = req.params.id
+    const { journal, title } = req.body
+    const contactLinks = await findContactLinks(journal)
+    console.log(contactLinks)
+    await Contact.updateMany({ journalLinks: id }, { $pull: { journalLinks: id } }, { multi: true })
+
+    const newContacts = await Contact.updateMany({ _id: { $in: contactLinks } }, { $push: { journalLinks: journal._id } }, { multi: true })
+    const newJournal = await Journal.updateOne({ _id: id }, { $set: { journal, title, contactLinks } }, { new: true })
+
+    try {
+      res.status(200).json({
+        newContacts,
+        newJournal
+      })
+      console.log(newContacts)
+      console.log(newJournal)
+    } catch (err) {
+      console.log(err)
+      res.status(400).json({ err: err.message })
+    }
+  },
+
+  delete: async (req, res) => {
+    const id = req.params.id
+    try {
+      Journal.deleteOne({ _id: id }, (err, journal) => {
+        if (err) return console.log(err)
+        res.status(200).json({ id })
+      })
+    } catch (err) {
+      console.log(err)
     }
   }
 }
